a5bbcec99cc0164d4db9b1a31ba045741a40c9bb
Jay Revolinsky <98365370+jayrevolinskyjr@users.noreply.github.com>
2023-02-16 10:18:13 -0800

    Add for-loop for static analysis test

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,12 @@ int main()
 {
 	std::cout << "Hello there!" << std::endl;
 
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << "General Kenobi!" << std::endl;
+	}
+
+	std::cout << "*coughs*" << std::endl;
+
 	return 0;
 }
